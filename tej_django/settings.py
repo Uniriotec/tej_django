95680646449ce71a1c1c6e55817432e5d3479c34
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-10-31 23:04:37 -0200

    colocando pasta media (onde ficam arquivos de upload de usuario), e setando no settings para apontar MEDIA_ROOT para essa pasta

--- tej_django/settings.py
+++ tej_django/settings.py
@@ -65,7 +65,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = LOCAL('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
