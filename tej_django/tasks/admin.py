935847a547d5d777a8961570fbfa0a50ab7118a2
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-01 00:11:25 -0200

    separando os campos de edição/add em sets(conjuntos) com fieldsets

--- tej_django/tasks/admin.py
+++ tej_django/tasks/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from tej_django.tasks.models import Task
 
 class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+           ('General',  {
+                'fields': ['label',]
+                }
+           ),
+           ('Status',  {
+                'fields': ['is_done',]
+                }
+           )
+        ]
 
     list_display = ('label','is_done')
 
