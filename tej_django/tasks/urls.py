89c6c5f8679380f2710613d537432f469d259a8c
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-02 11:14:34 -0200

    colocando url de edit de task

--- tej_django/tasks/urls.py
+++ tej_django/tasks/urls.py
@@ -5,4 +5,5 @@ from tej_django.tasks import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<task_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<task_id>\d+)/edit/$', views.edit, name='edit'),
 )
