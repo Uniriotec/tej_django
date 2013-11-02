7391e0746c6865f8ccbb9422fa73eb0ee66fc1aa
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-01 22:44:09 -0200

    utilizando shortcuts para renderizar templates

--- tej_django/tasks/views.py
+++ tej_django/tasks/views.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from tej_django.tasks.models import Task
 
 def index(request):
     tasks = Task.objects.all()
 
-    template = loader.get_template('task_index.html')
-    context = RequestContext(request, {
-        'tasks': tasks,
-    })
-    return HttpResponse(template.render(context))
+    return render_to_response('task_index.html',
+                {'tasks': tasks},
+                context_instance=RequestContext(request)
+           )
 
 def detail(request, task_id):
     return HttpResponse("Essa eh a task %s." % task_id)
