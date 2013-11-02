978944bfdac49142f145e0a9db1db2a5e23d1dc0
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-01 22:35:36 -0200

    utilizando templates, exemplo simples

--- tej_django/tasks/views.py
+++ tej_django/tasks/views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from tej_django.tasks.models import Task
 
 def index(request):
-    return HttpResponse("This is DJAAANNGOOO!!.")
+    tasks = Task.objects.all()
+
+    template = loader.get_template('task_index.html')
+    context = RequestContext(request, {
+        'tasks': tasks,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, task_id):
     return HttpResponse("Essa eh a task %s." % task_id)
