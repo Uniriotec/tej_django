a867684a468d44076626444a4f8160f1f42befad
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-02 11:15:10 -0200

    fazendo view responsavel pela edição. Sem usar forms

--- tej_django/tasks/views.py
+++ tej_django/tasks/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 
 from tej_django.tasks.models import Task
@@ -19,3 +19,13 @@ def detail(request, task_id):
                 {'task': task},
                 context_instance=RequestContext(request)
            )
+
+def edit(request, task_id):
+    task = get_object_or_404(Task,pk=task_id)
+
+    if request.method == 'POST':
+        is_done = request.POST.get('is_done',False)
+        task.is_done = is_done
+        task.save()
+
+    return redirect('tasks:detail',task_id=task_id)
