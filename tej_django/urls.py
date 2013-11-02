432b8d9d0d9d911a4aec11d4214bf7dc13fbf9c3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-01 22:19:37 -0200

    incluindo urls de tasks nas urls do projeto

--- tej_django/urls.py
+++ tej_django/urls.py
@@ -6,8 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tej_django.views.home', name='home'),
-    # url(r'^tej_django/', include('tej_django.foo.urls')),
+    url(r'^tasks/', include('tej_django.tasks.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
