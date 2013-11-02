b17d0e710810c5fae2f3c77cd7eaed72e04327dd
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-01 22:24:16 -0200

    criando view de detail de task e mapeando ela na urls

--- tej_django/tasks/urls.py
+++ tej_django/tasks/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from tej_django.tasks import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<task_id>\d+)/$', views.detail, name='detail'),
 )
--- tej_django/tasks/views.py
+++ tej_django/tasks/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is DJAAANNGOOO!!.")
+
+def detail(request, task_id):
+    return HttpResponse("Essa eh a task %s." % task_id)
