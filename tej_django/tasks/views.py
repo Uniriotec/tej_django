93d288db5c88e7fedf9539ca835420ff5fdd3d54
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-02 11:29:34 -0200

    mudando views de detail e de task para usarem o Model Form

--- tej_django/tasks/views.py
+++ tej_django/tasks/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 
 from tej_django.tasks.models import Task
+from tej_django.tasks.forms import TaskForm
+
 
 def index(request):
     tasks = Task.objects.all()
@@ -14,9 +16,13 @@ def index(request):
 
 def detail(request, task_id):
     task = get_object_or_404(Task,pk=task_id)
+    form = TaskForm(instance=task)
 
     return render_to_response('task_detail.html',
-                {'task': task},
+                {
+                'task': task,
+                'form':form
+                },
                 context_instance=RequestContext(request)
            )
 
@@ -24,8 +30,16 @@ def edit(request, task_id):
     task = get_object_or_404(Task,pk=task_id)
 
     if request.method == 'POST':
-        is_done = request.POST.get('is_done',False)
-        task.is_done = is_done
-        task.save()
+        form = TaskForm(request.POST,instance=task)
 
-    return redirect('tasks:detail',task_id=task_id)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:detail',task_id=task_id)
+
+    return render_to_response('task_detail.html',
+                {
+                    'task': task,
+                    'form':form
+                },
+                context_instance=RequestContext(request)
+           )
