47b69dd8c37861528739df3c06bc521602ff8e70
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-01 22:18:35 -0200

    removendo print do settings.py

--- tej_django/settings.py
+++ tej_django/settings.py
@@ -14,8 +14,6 @@ LOCAL = lambda x: os.path.join(os.path.sep.join(
                 os.path.abspath(
                     os.path.dirname(__file__)).split(os.path.sep)), x)
 
-print LOCAL('teste')
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
